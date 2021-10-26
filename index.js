5feca7feff9cd7e6c53229967037d0e9e16c8416
masum691 <masumhu691@gmail.com>
2021-10-26 13:01:52 +0600

    connect user and pass on env variable

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = 5000;
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('Connected to db')
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Genius car server');
