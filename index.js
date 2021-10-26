5c1749d692b332ebf2d13dcd44771b4fc21e055c
masum691 <masumhu691@gmail.com>
2021-10-26 12:11:44 +0600

    mongo client

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const { MongoClient } = require('mongodb');
 const app = express();
 const port = 5000;
 
+const uri = "mongodb+srv://<username>:<password>@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.get('/', (req, res) => {
-    res.send('Running genius server');
+    res.send('Genius car server');
 });
 
 app.listen(port, () => {
-    console.log('Running genius server on port', port)
+    console.log('Running genius server on port', port);
 });
+
+
