8c46e9813f701b26632dc353335e0e69cbb9fcf6
masum691 <masumhu691@gmail.com>
2021-10-26 18:14:44 +0600

    insert service data to the mongo cloud db

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors')
 require('dotenv').config()
+
+
 const app = express();
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri)
@@ -12,7 +19,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        console.log('Connected to db')
+        const database = client.db('carMechanic')
+        const servicesCollection = database.collection('services')
+
+        //  POST API
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service)
+            
+            const result = await servicesCollection.insertOne(service);
+            console.log(result)
+            res.send(result);
+        })
     }
     finally{
         // await client.close();
@@ -27,5 +45,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Running genius server on port', port);
 });
-
-
